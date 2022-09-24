39f9a0669f0605d1ae59d82f0b0433906dea3582
XUEHongjia <91336916+XUEHongjia@users.noreply.github.com>
2022-09-24 16:47:07 +0800

    Add files via upload

---
+++ ELEC3200hw1.m
@@ -0,0 +1,41 @@
+A = [ -1.7860, 15.9674, -1.6930, 13.1487;
+-1.1773, -15.2644, -1.3548, -14.5145;
+3.3427, -6.8798, 3.4513, -3.5889;
+1.5017, 16.7058, 1.1717, 13.4250 ];
+
+B = [ -0.9618
+1.3011;
+0.1081;
+1.1984 ];
+
+C = [ 29.8608, 35.9450, 29.4689, 17.7162 ];
+
+D = 0;
+
+%Q1
+sys = ss(A,B,C,D);
+
+%Q2
+sys2 = tf(sys);
+
+%Q3
+sys3 = zpk(sys);
+
+%Q4
+num = sys2.Numerator;
+den = sys2.Denominator;
+sys4 = tf(num,den);
+[A4, B4, C4, D4] = ssdata(sys4);
+
+%Q5
+[A5, B5, C5, D5] = ssdata(sys3);
+sys5 = ss(A5,B5,C5,D5)
+
+%testing 
+[y1,t1] = impulse(sys);
+[y4,t4] = impulse(sys4);
+[y5,t5] = impulse(sys5);
+dif14 = (y4-y1)'*(y4-y1);
+dif15 = (y5-y1)'*(y5-y1);
+
+clear t1 t4 t5
