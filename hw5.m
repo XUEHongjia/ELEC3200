81133a6f59dbd9b073ed091ffab06e211f9b107e
XUEHongjia <91336916+XUEHongjia@users.noreply.github.com>
2022-11-16 12:25:38 +0800

    Delete hw5.m

--- hw5.m
+++
@@ -1,5 +0,0 @@
-z=-1; %vector of zeros
-p=[0,-5,-10]; %vector of poles
-k=1; %gain (not the var iable gain K)
-L=zpk(z,p,k) %form the transfer function of the system
-rlocus(L) %generate a complementary root-locus plot with an automatically chosen 
